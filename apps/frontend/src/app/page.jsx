1082977aedf3c880d62302796464e851fbfcf94f
gaurav-132 <ggusain264@gmail.com>
2025-05-28 13:13:59 +0530

    Add frontend to the main branh

---
+++ apps/frontend/src/app/page.jsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <div className="flex justify-center h-[100vh] py-[500px] bg-red-900">Hello</div>
+    );
+}
